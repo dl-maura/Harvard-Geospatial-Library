905b4bfa3ee9cacf0a07d22c97c05d5f8de889a1
mferrarini <maura.ferrarini@gmail.com>
2021-02-25 16:14:43 -0500

    FE pattern testing

---
+++ app/assets/javascripts/bootstrap-accordion.js
@@ -0,0 +1,50 @@
+$(document).on('turbolinks:load', function () {
+
+  var windowWidth = $(window).width();
+  if (windowWidth <= 992) { //for iPad & smaller devices
+    $('#accordion .panel-collapse').collapse('hide');
+    $('#accordion .expand_caret').addClass('fa-rotate-180');
+  } else {
+    $('.toggle-all-facets .expand-text').hide();
+    $('.toggle-all-facets .collapse-text').show();
+    $('.toggle-all-facets').attr('aria-expanded', 'true');
+  }
+
+
+  $('.toggle-all-facets').on('click', function () {
+    console.log("clicked expand button");
+    if ($(this).attr('aria-expanded') === 'true') {
+      setToggleCollapsed();
+    } else {
+      $('.toggle-all-facets').attr('aria-expanded', 'true');
+      $('#accordion .panel-collapse').collapse('show');
+      $('.toggle-all-facets').find('.expand-text').hide();
+      $('.toggle-all-facets').find('.collapse-text').show();
+    }
+  });
+
+  $('#accordion .panel-collapse').on('show.bs.collapse', function () {
+    $(this).parent().find('.expand_caret').removeClass('fa-rotate-180');
+  });
+  $('#accordion .panel-collapse').on('hide.bs.collapse', function () {
+    $(this).parent().find('.expand_caret').addClass('fa-rotate-180');
+  });
+  $('#accordion .panel-collapse').on('hidden.bs.collapse', function () {
+    if ($('.facets__container .panel-collapse.in').length == 0) {
+      setToggleCollapsed();
+    }
+  });
+  $('#details dd.collapse').on('show.bs.collapse', function () {
+    $(this).prev().find('.field-toggle .fa').removeClass('fa-caret-down').addClass('fa-caret-up');
+  });
+  $('#details dd.collapse').on('hide.bs.collapse', function () {
+    $(this).prev().find('.field-toggle .fa').removeClass('fa-caret-up').addClass('fa-caret-down');
+  });
+});
+
+function setToggleCollapsed() {
+  $('.toggle-all-facets').attr('aria-expanded', 'false');
+  $('#accordion .panel-collapse').collapse('hide');
+  $('.toggle-all-facets').find('.expand-text').show();
+  $('.toggle-all-facets').find('.collapse-text').hide();
+}
