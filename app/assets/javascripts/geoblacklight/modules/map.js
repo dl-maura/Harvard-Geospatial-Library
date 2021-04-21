54108187160e0682149061411169f5fbff98e6f2
mferrarini <maura.ferrarini@gmail.com>
2021-04-21 14:56:19 -0400

    modified bounding box

--- app/assets/javascripts/geoblacklight/modules/map.js
+++ app/assets/javascripts/geoblacklight/modules/map.js
@@ -1,9 +1,8 @@
 // modify map viewer behaviors
 
 GeoBlacklight.Viewer.Map = GeoBlacklight.Viewer.Map.extend({
-
   // override initial bbox
   options: {
-      bbox: [[-40, -90],[60, 50]]
+    bbox: [[-60,-60],[100,80]]
   }
 });
